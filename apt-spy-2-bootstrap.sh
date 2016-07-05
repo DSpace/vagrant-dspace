06abaa8d641bdfd447f0fd81582e3baa4ccc19a1
Hardy Pottinger <pottingerhj@missouri.edu>
2016-07-05 10:54:18 -0500

    [DS-3260] increased librarian-puppet version to 2.2.3, and switched to Ruby 2 in apt-spy-2-bootstrap

--- apt-spy-2-bootstrap.sh
+++ apt-spy-2-bootstrap.sh
@@ -13,11 +13,11 @@ echo "Installing 'apt-spy2'. This tool lets us autoconfigure your 'apt' sources.
 echo "  This may take a while..."
 
 # Ensure dependencies are installed (These are needed to dynamically determine your country code).
-# * ruby >= 1.9.2 is needed for apt-spy2
+# * ruby >= 2.0 is needed for apt-spy2
 # * zlib1g-dev (zlib) is needed to build apt-spy2 from "native extensions" (needed to install "nokogiri" prerequisite)
 # * dnsutils ensures 'dig' is installed (to get IP address)
 # * geoip-bin ensures 'geoiplookup' is installed (lets us look up country code via IP)
-sudo apt-get install -y ruby1.9.3 zlib1g-dev dnsutils geoip-bin >/dev/null
+sudo apt-get install -y ruby2.0 zlib1g-dev dnsutils geoip-bin >/dev/null
 
 # Install/Update RubyGems for the provider
 echo "Installing RubyGems..."
