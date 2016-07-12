3358e95a1da21dd616360794a22a6c48d07dde91
Tim Donohue <tdonohue@duraspace.org>
2016-07-12 15:59:44 -0500

    Bug fix. Try another apt mirror if first one fails.

--- apt-spy-2-bootstrap.sh
+++ apt-spy-2-bootstrap.sh
@@ -36,21 +36,31 @@ export COUNTRY=`geoiplookup $CURRENTIP | awk -F: '{ print $2 }' | awk -F, '{ pri
 
 #If country code is empty or != 2 characters, then use "US" as a default
 if [ -z "$COUNTRY" ] || [ "${#COUNTRY}" -ne "2" ]; then
-   COUNTRY = "US"
+   COUNTRY="US"
 fi
 
 if [ "$(gem search -i apt-spy2)" = "false" ]; then
   echo "Installing apt-spy2 (and prerequisites)..."
   gem install --no-ri --no-rdoc apt-spy2
   echo "... apt-spy2 installed!"
-  echo "... Setting 'apt' sources.list for closest mirror to country=$COUNTRY"
-  sudo apt-spy2 fix --launchpad --commit --country=$COUNTRY
-else
-  echo "... Setting 'apt' sources.list for closest mirror to country=$COUNTRY"
-  sudo apt-spy2 check
-  sudo apt-spy2 fix --launchpad --commit --country=$COUNTRY
 fi
+
+echo "... Setting 'apt' sources.list for closest mirror to country=$COUNTRY"
+sudo apt-spy2 check
+# By default lookup a mirror using launchpad.net
+sudo apt-spy2 fix --launchpad --commit --country=$COUNTRY
 
 # apt-spy2 requires running an 'apt-get update' after doing a 'fix'
 echo "Re-running apt-get update after sources updated..."
+set +e  #temporarily ignore errors
 sudo apt-get update >/dev/null
+RESULT=$?
+set -e  # reenable exit on error
+
+# If previous apt-get errored out, re-run apt-spy2 with ubuntu list of mirrors (i.e. not launchpad)
+if [ $RESULT -ne 0 ]; then
+  echo "Initial apt-get update failed. Trying a different mirror as a fallback..."
+  sudo apt-spy2 fix --commit --country=$COUNTRY
+  echo "Re-running apt-get update after sources updated (again)..."
+  sudo apt-get update >/dev/null
+fi
